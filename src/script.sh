cb5bedcc321c63483325e31348725a528ebf8060
therealdelay <danny.a_soares@hotmail.com>
2018-03-16 16:52:02 +0000

    all done, i guess

--- src/script.sh
+++ src/script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+jjtree yal2jvm.jjt
 javacc yal2jvm.jj
 javac *.java
-java yal2jvm
+#java yal2jvm aval1_err
+ java yal2jvm aval2_err
+# java yal2jvm aval3_err
+# java yal2jvm aval4_err
+# java yal2jvm aval5_err
+# java yal2jvm aval6_err
+# java yal2jvm aval7_err
+# java yal2jvm array2_err
+# java yal2jvm array4_err
