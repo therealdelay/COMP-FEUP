c398854ca336c31d671aeeb20ff5e56290acfe41
therealdelay <danny.a_soares@hotmail.com>
2018-04-19 11:20:39 +0100

    a

--- src/ASTFunction.java
+++ src/ASTFunction.java
@@ -2,13 +2,14 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTFunction extends SimpleNode {
-  public ASTFunction(int id) {
-    super(id);
-  }
 
-  public ASTFunction(yal2jvm p, int id) {
-    super(p, id);
-  }
+	public ASTFunction(int id) {
+		super(id);
+	}
+
+	public ASTFunction(yal2jvm p, int id) {
+		super(p, id);
+	}
 
 }
 /* JavaCC - OriginalChecksum=960d7fdeabca8f1d6cc1627ee5a17f15 (do not edit this line) */
