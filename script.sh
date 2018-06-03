f8e75111732a027c0b835448c4944e86d27dec74
leonardomgt <leogt-15@hotmail.com>
2018-06-03 18:48:38 +0100

    nada

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-mkdir -p bin
+ 
 jjtree -OUTPUT_DIRECTORY=src/yal src/yal/yal2jvm.jjt
 javacc -OUTPUT_DIRECTORY=src/yal src/yal/yal2jvm.jj
+mkdir -p bin
 javac -d bin src/yal/*.java
 cd bin
 java yal.yal2jvm $1
